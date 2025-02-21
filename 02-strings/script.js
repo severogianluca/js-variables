f9c200ae4a236cce346108488892181311f80f28
severogianluca <gianlucasevero300@gmail.com>
2025-02-21 18:37:28 +0100

    fix

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,21 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const name = 'Gianluca';
+console.log(name)
+
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 10
+const text = 'Roma'
+const bool = 'true'
+
+console.log(number)
+console.log(text)
+console.log(bool)
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -23,17 +33,8 @@ const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
 
 
-// punto 1
-const name = 'Gianluca';
-console.log(name)
 
 
-// punto 2
-const number = 10
-const text = 'Roma'
-const bool = 'true'
 
-console.log(number)
-console.log(text)
-console.log(bool)
+
 
